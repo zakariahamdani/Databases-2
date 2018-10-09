ba8d23842b08024a7863d2e8d5f5c1906721bb7e
Stefan T. Ruehl <stefan.ruehl@h-da.de>
2018-10-09 16:00:37 +0200

    fix JPA with Oracle driver 8 and older eclipselink version

--- src/main/java/de/hda/fbi/db2/stud/Main.java
+++ src/main/java/de/hda/fbi/db2/stud/Main.java
@@ -20,6 +20,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+
         try {
             //Read default csv
             final List<String[]> defaultCsvLines = CsvDataReader.read();
