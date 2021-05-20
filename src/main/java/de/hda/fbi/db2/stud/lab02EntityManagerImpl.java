ad8b09c72ec75c3f038c6822d39face2225212a4
istzahamd <Zakaria.Hamdani@stud.h-da.de>
2021-05-21 00:49:15 +0200

    [code] implemented lab02EntityManagerImpl

---
+++ src/main/java/de/hda/fbi/db2/stud/lab02EntityManagerImpl.java
@@ -0,0 +1,33 @@
+package de.hda.fbi.db2.stud;
+import de.hda.fbi.db2.api.Lab02EntityManager;
+import de.hda.fbi.db2.stud.entity.Category;
+import de.hda.fbi.db2.stud.entity.Question;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class lab02EntityManagerImpl extends Lab02EntityManager{
+
+  private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("docker-local-postgresPU");
+  private static EntityManager em = emf.createEntityManager();
+  @Override
+  public void persistData() {
+      em.getTransaction().begin();
+
+      for(Question question : this.lab01Data.getQuestions()){
+        em.persist(question);
+      }
+
+      for (Category category : this.lab01Data.getCategories()){
+        em.persist(category);
+      }
+
+      em.getTransaction().commit();
+      em.close();
+  }
+
+  @Override
+  public EntityManager getEntityManager() {
+    return em;
+  }
+}
